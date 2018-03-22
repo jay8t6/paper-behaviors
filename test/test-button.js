b5c9dff4128234220612f30ea0819f4816a403f6
Fred K. Schott <fschott@google.com>
2018-03-22 11:38:19 -0500

    auto-converted by polymer-modulizer

--- test/test-button.js
+++ test/test-button.js
@@ -1,4 +1,9 @@
-<!--
+import '@polymer/polymer/polymer-legacy.js';
+import { PaperButtonBehavior } from '../paper-button-behavior.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+
+/**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -6,29 +11,15 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
-
-<link rel="import" href="../../polymer/polymer.html">
-<link rel="import" href="../paper-button-behavior.html">
-
-<dom-module id="test-button">
-
-  <template>
+*/
+Polymer({
+  _template: html`
     <slot></slot>
-  </template>
+`,
 
-  <script>
+  is: 'test-button',
 
-    Polymer({
-
-      is: 'test-button',
-
-      behaviors: [
-        Polymer.PaperButtonBehavior
-      ]
-
-    });
-
-  </script>
-
-</dom-module>
+  behaviors: [
+    PaperButtonBehavior
+  ]
+});
